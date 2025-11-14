bcf673b4793a1f5353825e6cfc8bb7dc50dc4c9c
tetsunikata-cmyk <tetsunikata@gmail.com>
2025-11-14 12:38:22 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,5 @@
 class Item < ApplicationRecord
   belongs_to :user
-   # 画像は ActiveStorage を使う
   has_one_attached :image
 
   # ActiveHash を使うカラム（あとでファイルを作る）
@@ -10,4 +9,34 @@ class Item < ApplicationRecord
   # belongs_to :postage_type
   # belongs_to :prefecture
   # belongs_to :shipping_day
+
+  # ⭐ 必須項目のバリデーション
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    validates :category_id
+    validates :condition_id
+    validates :postage_type_id
+    validates :prefecture_id
+    validates :shipping_day_id
+    validates :price
+  end
+
+  # ⭐ ActiveHash（「---」＝ id:1 を選べないようにする）
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :category_id
+    validates :condition_id
+    validates :postage_type_id
+    validates :prefecture_id
+    validates :shipping_day_id
+  end
+
+  # ⭐ 価格の範囲・整数チェック
+  validates :price,
+            numericality: {
+              only_integer: true,
+              greater_than_or_equal_to: 300,
+              less_than_or_equal_to: 9_999_999
+            }
 end
