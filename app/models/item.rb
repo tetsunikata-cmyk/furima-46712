fa7040ea286472c85970847781f0299477ae2194
tetsunikata-cmyk <tetsunikata@gmail.com>
2025-11-14 20:37:03 +0900

    エラーハンドリングの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
+  # アソシエーション
   belongs_to :user
   has_one_attached :image
 
@@ -10,20 +11,15 @@ class Item < ApplicationRecord
     belongs_to :prefecture
     belongs_to :shipping_day
 
-  # ⭐ 必須項目のバリデーション
+  # 必須項目のバリデーション
   with_options presence: true do
     validates :image
     validates :name
     validates :description
-    validates :category_id
-    validates :condition_id
-    validates :postage_type_id
-    validates :prefecture_id
-    validates :shipping_day_id
-    validates :price
+     validates :price
   end
 
-  # ⭐ ActiveHash（「---」＝ id:1 を選べないようにする）
+  # ActiveHash（「---」＝ id:1 を選べないようにする）
   with_options numericality: { other_than: 1, message: "can't be blank" } do
     validates :category_id
     validates :condition_id
@@ -32,11 +28,12 @@ class Item < ApplicationRecord
     validates :shipping_day_id
   end
 
-  # ⭐ 価格の範囲・整数チェック
+  # 価格の範囲・整数チェック
   validates :price,
             numericality: {
               only_integer: true,
               greater_than_or_equal_to: 300,
               less_than_or_equal_to: 9_999_999
-            }
+            },
+            allow_blank: true  
 end
