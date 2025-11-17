455bc2da168345cc198aaf01d8c043cbe47ec933
tetsunikata-cmyk <tetsunikata@gmail.com>
2025-11-17 15:35:46 +0900

    再テスト

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,23 +1,14 @@
 class OrderAddress
-  
   include ActiveModel::Model
 
   attr_accessor :postal_code, :prefecture_id, :city, :address,
                 :building, :phone_number, :token, :user_id, :item_id
 
-  # =========================
-  # バリデーション
-  # =========================
-
-  # presence: true をまとめてかけるグループ
   with_options presence: true do
-  
     validates :postal_code, format: {
       with: /\A\d{3}-\d{4}\z/,
       message: 'is invalid. Include hyphen(-)'
     }
-
-    # 市区町村・番地・電話番号・token・user_id・item_id はとにかく必須
     validates :city
     validates :address
     validates :phone_number, format: {
@@ -29,22 +20,14 @@ class OrderAddress
     validates :item_id
   end
 
-  # 都道府県（ActiveHash）：1（---）は選べないように
   validates :prefecture_id, numericality: {
     other_than: 1,
     message: "can't be blank"
   }
 
-  # building は任意なのでバリデーション不要
-
-  # =========================
-  # DB保存処理
-  # =========================
   def save
-    # ① 購入情報を orders テーブルに保存
     order = Order.create(user_id: user_id, item_id: item_id)
 
-    # ② 住所情報を addresses テーブルに保存
     Address.create(
       postal_code:   postal_code,
       prefecture_id: prefecture_id,
