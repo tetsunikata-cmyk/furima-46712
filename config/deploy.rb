c28410ad492f00cd0604bf76c14791bf55155fdc
tetsunikata-cmyk <tetsunikata@gmail.com>
2025-12-02 19:29:01 +0900

    unicornの設定ファイルを編集

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,18 +5,18 @@ lock '3.19.2'
 set :application, 'ご自身のアプリケーションfurima-46712'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:Githubのユーザー名/レポジトリ名.git'
+set :repo_url,  'git@github.com:tetsunikata-cmyk/furima-46712.git'
 set :branch, 'main'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, 'このアプリで使用しているrubyのバージョン' #カリキュラム通りに進めた場合、’3.2.0’ です
+set :rbenv_ruby, '3.2.0' #カリキュラム通りに進めた場合、’3.2.0’ です
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                                  keys: ['~/.ssh/ご自身のキーペア名.pem'] 
+                                  keys: ['~/.ssh/my-key-pair.pem'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
